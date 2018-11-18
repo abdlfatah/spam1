6a5cc0238d8558373788e512f917ae9e6d0022b4
abdlfatah <45146141+abdlfatah@users.noreply.github.com>
2018-11-18 19:35:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "511915174903414784"; // ايدي السررفر
+var channel = "513747061766750209";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client.guilds.get(server).channels.get(channel).send('**ايرون مان , ايرون مان , ايرون مان , ايرون مان , ايرون مان , ايرون مان , ايرون مان , ايرون مان , ايرون مان , ايرون مان , ايرون مان , ايرون مان , ايرون مان , ايرون مان , ايرون مان , ايرون مان , ايرون مان , ايرون مان , **')
     },305);
 })
 
